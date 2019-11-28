010348e2afc93c3fa02b6d94d278d4fd49e2cc9c
Alejandro Tardín <alejandro.tardin@liferay.com>
2019-11-28 13:34:35 +0100

    Steps 1-7

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useCallback} from 'react';
+import './App.css'
+import debounce from 'debounce'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Amount extends React.Component {
+  render() {
+    return (
+      <label className={this.props.value < 0 ? 'error' : ''}>
+        {this.props.name}: 
+        <input 
+          type="number" 
+          value={this.props.value}
+          disabled={!this.props.onChange}
+          onChange={
+            this.props.onChange 
+              ? e => this.props.onChange(parseFloat(e.target.value)) 
+              : null
+          }          
+        />
+      </label>
+    )
+  }
 }
 
-export default App;
+export default function App() {
+  const [euros, setEuros] = useState(1000)
+  const [exchangeRate, setExchangeRate] = useState(Math.random() * 1000)
+  const debouncedExchangeRate = useCallback(debounce(setExchangeRate, 1000), [])
+  return (
+    <>
+      <Amount 
+        name="Euros" 
+        value={euros} 
+        onChange={euros => {                    
+          debouncedExchangeRate(0);
+          setEuros(euros);
+        }} 
+      />
+      <Amount 
+        name="$BTC" 
+        value={euros * exchangeRate}        
+      />
+      {exchangeRate ===  0 && (
+        <button onClick={() => setExchangeRate(Math.random() * 1000)}>
+          Recover BTC Market
+        </button>
+      )}
+    </>
+  )
+}
